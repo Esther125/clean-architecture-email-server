3978592fc0a392f28064fe14176d3aab5a041702
Esther911205 <esther.li@mile.cloud>
2024-08-06 15:17:14 +0800

    fix: degrade python version, add dependency-injector

--- tests/adapter/inward/web/send_email/test_send_email_controller.py
+++ tests/adapter/inward/web/send_email/test_send_email_controller.py
@@ -7,7 +7,7 @@ class TestSendEmailEndpoint(TestCase):
     def setUp(self):
         self.client = TestClient(app)
 
-    def test_dispatch_email(self):
+    def test_queue_and_save_email(self):
         response = self.client.post(
             "/v1/user/email",
             headers={"Content-Type": "application/json"},
@@ -22,7 +22,10 @@ class TestSendEmailEndpoint(TestCase):
         assert result["email_id"], "email_id should not be null"
         assert isinstance(result["email_id"], str), "email_id should be a string"
 
-    def test_deliver_email(self):
+    def test_queue_and_save_email_failed(self):
+        pass
+
+    def test_send_and_update_email(self):
         response = self.client.post(
             "/v1/queue/email",
             headers={"Content-Type": "application/json"},
@@ -37,3 +40,6 @@ class TestSendEmailEndpoint(TestCase):
         result = response.json()
         assert result["email_id"], "email_id should not be null"
         assert isinstance(result["email_id"], str), "email_id should be a string"
+
+    def test_send_and_update_email_failed(self):
+        pass
