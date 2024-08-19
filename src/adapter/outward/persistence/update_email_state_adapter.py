5f6e1b4e70c0028651244260f1789a512611c55d
Esther911205 <esther.li@mile.cloud>
2024-08-19 09:57:26 +0800

    feat: implement update email state adapter

--- src/adapter/outward/persistence/update_email_state_adapter.py
+++ src/adapter/outward/persistence/update_email_state_adapter.py
@@ -1,3 +1,8 @@
+import os
+from google.cloud import firestore
+from google.cloud.firestore_v1 import FieldFilter
+
+
 from src.app.port.outward.update_email_state.update_email_state_command import (
     UpdateEmailStateCommand,
 )
@@ -8,7 +13,14 @@ from src.app.port.outward.update_email_state.update_email_state_port import (
 
 class UpdateEmailStateAdapter(UpdateEmailStatePort):
     def __init__(self) -> None:
-        pass
+        self.project_id = os.getenv("GCP_PROJECT_ID")
+        self.database_id = os.getenv("GCP_FIRESTORE_DATABASE_ID")
+        self.db = firestore.AsyncClient(self.project_id, self.database_id)
 
     async def update_state(self, command: UpdateEmailStateCommand) -> None:
-        pass
+        doc_ref = (
+            self.db.collection("emails")
+            .where(filter=FieldFilter("email_id", "==", command.email_id))
+            .stream()
+        )
+        await doc_ref.update({"is_sent": command.is_sent})
